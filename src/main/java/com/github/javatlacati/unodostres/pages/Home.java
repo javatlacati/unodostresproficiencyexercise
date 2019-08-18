be4f3dbf83bf9469f8ae95c338531808b089a7c2
Javatlacati <scherzo_16@hotmail.com>
2019-08-18 03:07:49 -0500

    Initial commit

---
+++ src/main/java/com/github/javatlacati/unodostres/pages/Home.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.javatlacati.unodostres.pages;
+
+/**
+ *
+ * @author Ruslan López Carro <scherzo16 at gmail.com>
+ */
+public class Home {
+    
+}
